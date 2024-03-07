b9635ae39b101c4fcae659c9a53a167472749601
Magnus Karström <mrkarstrom81@gmail.com>
2024-03-07 05:47:13 +0100

    add (TextBlock.tsx): Added the conditional rendering of a textblock.

--- src/Components/Accordion.tsx
+++ src/Components/Accordion.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import TextBlock from './TextBlock';
 
 const Accordion = () => {
   const [isToggled, setIsToggled] = useState(false);
 
   return (
     <div>
-      {isToggled && <h3>Show Me</h3>}
-      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+      {/* {isToggled && <h3>Show Me</h3>} */}
+      {isToggled ? <TextBlock /> : null}
+      <button onClick={() => setIsToggled(!isToggled)}>
+        {isToggled ? <span>Close</span> : <span>Open</span>}
+      </button>
     </div>
   );
 };
---
+++ src/Components/TextBlock.tsx
@@ -0,0 +1,40 @@
+const TextBlock = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+      praesentium tenetur veniam explicabo? Quis sit nam temporibus obcaecati
+      impedit porro, quia neque! Iusto earum doloremque eligendi amet harum
+      voluptates voluptate cum! Qui est voluptatem consequuntur iure asperiores
+      obcaecati facere, nisi, eos cum sint consectetur fugit dolore esse
+      possimus dolores deleniti eligendi maxime minima impedit animi soluta.
+      Atque iusto eos molestiae accusamus optio, sequi, ex sapiente recusandae
+      repudiandae qui ipsam molestias veritatis porro dolores, officiis possimus
+      unde fugit modi omnis cumque nobis dolorem tempore maxime. Soluta neque
+      reprehenderit, totam sit omnis dicta nostrum quia aperiam voluptatum nihil
+      expedita porro minus. Vitae consequatur, aliquam nesciunt saepe eos
+      architecto, inventore facilis pariatur consequuntur nulla fuga perferendis
+      quo placeat fugiat itaque error ad reprehenderit repellat odio cupiditate
+      in maiores natus dolore quis? Sapiente vero molestias facere ullam itaque
+      vitae illum error veniam eos asperiores, officiis pariatur. Placeat,
+      consequuntur. Eveniet esse eos assumenda at molestiae quaerat totam libero
+      blanditiis, vero inventore sed eum ullam atque minus perspiciatis?
+      Eligendi laboriosam beatae temporibus voluptatem consequuntur architecto
+      minima, alias, totam at, soluta facilis minus blanditiis quia eveniet
+      similique dolores facere veniam ea. Quam minus aut voluptatem sed
+      deserunt? Ratione sit, ex temporibus quaerat esse eveniet inventore
+      consectetur facilis voluptas molestias magni voluptates labore, quod, hic
+      mollitia culpa? Atque molestias, corrupti quod tempore odit rerum
+      possimus, hic, aliquam laudantium alias consequuntur nam! Repellat quasi
+      consequuntur id maiores magnam. Incidunt eos voluptate fuga unde nulla a
+      id sequi suscipit? Soluta, incidunt ullam debitis nisi culpa velit non,
+      eum quis laudantium earum ipsa sapiente error ad repellat. Veritatis saepe
+      minima alias officia quam! Recusandae rerum beatae nam ipsam deleniti.
+      Debitis minus ducimus culpa, dicta odit dolores delectus, alias eaque
+      dolorem consequuntur recusandae enim non at iure quos placeat dolore nemo,
+      adipisci eveniet voluptate maiores nesciunt. Inventore quod nam in
+      blanditiis necessitatibus.
+    </div>
+  );
+};
+
+export default TextBlock;
